c90bb12894ca2f298a7571eeec8c745bf83302eb
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-12-10 17:45:32 +0100

    changes

--- src/pages/transactions/Withdraw.tsx
+++ src/pages/transactions/Withdraw.tsx
@@ -18,8 +18,6 @@ const Withdraw: React.FC = () => {
           },
         });
         setUser(response.data);
-        console.log(user);
-        
       } catch (error: any) {
         toast.error("Failed to fetch user details");
       }
@@ -71,52 +69,59 @@ const Withdraw: React.FC = () => {
   });
 
   const banks = [
-    "Select a bank", "ABN AMRO", "Absa Group", "Agricultural Bank of China", "ANZ Banking Group", 
-    "Axis Bank", "Banco Bradesco", "Banco de la Nación Argentina", 
-    "Banco do Brasil", "Banco Santander", "Banco Santander Río", "Banamex", 
-    "Bank of America", "Bank of China", "Bank of East Asia", "Bank of Montreal", 
-    "Barclays", "BBVA", "BBVA Bancomer", "Booster Bank", "BNP Paribas", "CIBC", "China Construction Bank", 
-    "CIMB Group", "Citigroup", "Commerzbank", "Commonwealth Bank", "Credit Suisse", 
-    "Crédit Agricole", "Danske Bank", "DBS Bank", "Deutsche Bank", "Ecobank", 
-    "FirstRand", "Guaranty Trust Bank", "Hang Seng Bank", "HDFC Bank", "HSBC", 
-    "ICICI Bank", "ING Group", "Intesa Sanpaolo", "Itaú Unibanco", 
-    "JPMorgan Chase", "Kiwibank", "Lloyds Banking Group", "Malayan Banking Berhad (Maybank)", 
-    "Mitsubishi UFJ Financial Group", "Mizuho Financial Group", "National Australia Bank", 
-    "National Bank of Canada", "Nedbank", "Nordea", "OCBC Bank", "Public Bank Berhad", 
-    "Royal Bank of Canada", "Royal Bank of Scotland", "Santander Brasil", 
-    "Scotiabank", "SEB", "Société Générale", "Standard Bank", "Standard Chartered", 
-    "State Bank of India", "Sumitomo Mitsui Financial Group", "SBI Holdings", 
-    "Toronto-Dominion Bank", "UBS", "UniCredit", "United Overseas Bank", 
-    "Wells Fargo", "Westpac", "Zenith Bank",
-    "Bank of New York Mellon", "Goldman Sachs", "Morgan Stanley", "Capital One", "American Express", "PNC Financial Services", "U.S. Bancorp", "SunTrust Banks", "Fifth Third Bank",
-    "Regions Financial Corporation", "KeyCorp", "M&T Bank", "Huntington Bancshares", "BB&T", "Ally Financial", "Santander Bank", "BMO Harris Bank", "Union Bank", "First Republic Bank",
-    "Bank of the West", "Citizens Financial Group", "Comerica", "New York Community Bancorp", "Zions Bancorporation", "People's United Financial", "Synovus Financial", "First Hawaiian Bank", "East West Bank", "Cullen/Frost Bankers",
-    "City National Bank", "BankUnited", "Associated Banc-Corp", "Valley National Bank", "First Interstate BancSystem", "Western Alliance Bank", "Old National Bank", "Webster Bank", "Umpqua Bank", "Fulton Financial Corporation",
-    "PacWest Bancorp", "Washington Federal", "Columbia Bank", "Texas Capital Bank", "Home BancShares", "Hancock Whitney Bank", "South State Bank", "Great Western Bank",
-    "BancorpSouth Bank", "Atlantic Union Bank", "Pinnacle Financial Partners", "Arvest Bank", "First Citizens Bank", "Bank OZK", "First Financial Bank", "Prosperity Bank", "First National Bank of Omaha", "Cadence Bank",
-    "Heartland Financial USA", "Trustmark National Bank", "UMB Bank", "FNB Corporation", "Simmons Bank", "TowneBank", "NBT Bank", "First Bank", "Glacier Bancorp", "First Community Bank",
-    "Horizon Bank", "Tri Counties Bank", "Old Point National Bank", "Sterling National Bank", "Bank of Marin", "Peoples Bank", "First Merchants Bank", "Peoples State Bank", "PlainsCapital Bank",
-    "Texas Bank and Trust", "United Community Bank", "Centier Bank", "Investors Bank", "S&T Bank", "ServisFirst Bank", "Citywide Banks", "Bank of Hawaii", "Bank of Springfield",
-    "FirstBank Holding Co.", "Pacific Premier Bank", "Renasant Bank", "Bankers Trust", "MidFirst Bank", "Haverhill Bank", "TIAA Bank", "Sandy Spring Bank", "Chemical Bank", "Flagstar Bank",
-    "Nicolet National Bank", "Pinnacle Bank", "MidWestOne Bank", "First National Bank and Trust", "Independent Bank", "Mechanics Bank", "Inland Bank and Trust", "Third Federal Savings and Loan", "Androscoggin Bank", "First Mid Bank & Trust",
-    "First National Bank Alaska", "Farmers & Merchants Bank", "First National Bank in Sioux Falls", "Pioneer Bank", "American National Bank", "Southwest Bank", "Peoples Bank of Alabama", "Peoples National Bank", "Great Southern Bank",
-    "City Bank", "Summit Bank", "Colony Bank", "Seacoast Bank", "First Southern Bank", "First State Bank", "Guaranty Bank", "American State Bank", "Bank of Commerce", "American Heritage Bank",
-    "Central Bank", "Stock Yards Bank & Trust", "BankFirst", "First Mid-Illinois Bank & Trust", "Republic Bank", "Bridgewater Bank", "Third Coast Bank", "American Bank of Commerce", "First Choice Bank", "Farmers State Bank",
-    "First Liberty National Bank", "First Savings Bank", "MainSource Bank", "Centennial Bank", "City National Bank of Florida", "First International Bank & Trust", "First Financial Bankshares",
-    "First United Bank", "First Western Trust Bank", "HomeStreet Bank", "Independent Bank Group", "Lakeland Bank", "LegacyTexas Bank", "Merchants Bank", "MidWestOne Financial Group", "Pacific Continental Bank",
-    "Peoples Security Bank & Trust", "Southside Bank", "Southern First Bank", "Summit State Bank", "Triumph Bank", "United Bank", "United National Bank",
-    "University Federal Credit Union", "Westamerica Bank", "Western State Bank", "Wilson Bank & Trust", "Wings Financial Credit Union", "Woodforest National Bank", "ZB, National Association", "Zions First National Bank", "1st Source Bank", "Alerus Financial",
-    "Altabank", "American First National Bank", "Ameris Bank", "Bank of Ann Arbor", "Bank of Clarke County", "Bank of George", "Bank of Idaho", "Bank of Labor", "Bank of Missouri", "Bank of San Francisco", 
-    "Bank of Stockton", "BankPlus", "Bremer Bank", "Bridge Bank", "Byline Bank", "California Bank & Trust", "Cambridge Trust Company", "Camden National Bank", "Capital City Bank", "Capitol Federal Savings Bank", 
-    "Cardinal Bank", "Carter Bank & Trust", "Cathay Bank", "Central Bank of Boone County", "Central Bank of the Midwest", "Central Pacific Bank", "CIBC Bank USA", "City National Bank of West Virginia", 
-    "Community Bank", "Community Bank, N.A.", "Community First Bank", "Community Trust Bank", "ConnectOne Bank", "Consumers Credit Union", "Crestmark Bank", "Dacotah Bank", "Dollar Bank", "Eastern Bank", 
-    "Enterprise Bank & Trust", "ESL Federal Credit Union", "Evolve Bank & Trust", "Farm Bureau Bank", "Fidelity Bank", "Financial Federal Bank", "First American Bank", "First Bank Financial Centre", 
-    "First Citizens Community Bank", "First Commonwealth Bank", "First Federal Bank", "First Midwest Bank", "First Republic Bank", "First Security Bank", "Frost Bank", "Fulton Bank", "Gateway First Bank", 
-    "Great Western Bank", "Hanmi Bank", "Hawaii National Bank", "Heartland Bank and Trust Company", "Heritage Bank", "HSBC Bank USA", "Huntington National Bank", "IberiaBank", "International Bank of Commerce", 
-    "Intrust Bank", "John Marshall Bank", "KeyBank", "Lake City Bank", "Liberty Bank", "Luther Burbank Savings", "Macatawa Bank", "Mercantile Bank of Michigan", "Midland States Bank", "New York Community Bank", 
-    "Northwest Bank", "OneWest Bank", "Origin Bank", "Peoples United Bank", "Popular Bank", "Rabo AgriFinance", "Raymond James Bank", "River City Bank", "Rockland Trust", "State Bank of Cross Plains", 
-    "Sterling Bank & Trust", "Sunflower Bank", "Tri Counties Bank", "TriState Capital Bank", "Union Bank", "Unity Bank", "Veritex Community Bank", "Wintrust Bank"
-  ];
+    "Select a bank",
+    "Access Bank Plc",
+    "Accion Microfinance Bank",
+    "Citibank Nigeria Limited",
+    "Coronation Merchant Bank",
+    "Ecobank Nigeria Plc",
+    "Empire Trust Microfinance Bank",
+    "FBNQuest Merchant Bank",
+    "Fidelity Bank Plc",
+    "First Bank of Nigeria Limited",
+    "First City Monument Bank Plc",
+    "Finca Microfinance Bank",
+    "FSDH Merchant Bank",
+    "Globus Bank Limited",
+    "Guaranty Trust Bank Plc",
+    "Heritage Banking Company Limited",
+    "Infinity Microfinance Bank",
+    "Jaiz Bank Plc",
+    "Keystone Bank Limited",
+    "Kuda Bank",
+    "LAPO Microfinance Bank",
+    "Lotus Bank Limited",
+    "Moniepoint",
+    "Nova Merchant Bank",
+    "Opay",
+    "PalmPay",
+    "Paga",
+    "Parallex Bank Limited",
+    "Peace Microfinance Bank",
+    "Polaris Bank Limited",
+    "Premium Trust Bank",
+    "Providus Bank Limited",
+    "Rand Merchant Bank",
+    "Rephidim Microfinance Bank",
+    "Signature Bank Limited",
+    "Stanbic IBTC Bank Plc",
+    "Standard Chartered Bank",
+    "Sterling Bank Plc",
+    "Sterling Financial Holdings",
+    "SunTrust Bank Nigeria Limited",
+    "Taj Bank Limited",
+    "Titan Trust Bank Limited",
+    "Union Bank of Nigeria Plc",
+    "United Bank for Africa Plc",
+    "Unity Bank Plc",
+    "VFD Microfinance Bank",
+    "Wema Bank Plc",
+    "Zenith Bank Plc",
+    "Flutterwave",
+    "Paystack",
+    "PiggyVest",
+    "PocketApp by PiggyVest"
+];
 
 
   return (
@@ -218,7 +223,7 @@ const Withdraw: React.FC = () => {
             onBlur={formik.handleBlur}
           >
             {banks.map((bank, index) => (
-              <option key={index} value={bank}>
+              <option disabled={bank === "Select a bank" ? true : false} key={index} value={bank}>
                 {bank}
               </option>
             ))}
